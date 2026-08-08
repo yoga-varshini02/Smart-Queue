8fd22ea8da6010e0bc091ea1678705df5ea03e52
Yoga Varshini <https://github.com/yoga-varshini02>
2026-08-08 22:18:47 +0530

    Complete backend

---
+++ backend/src/main/java/com/smartqueue/SmartqueueApplication.java
@@ -0,0 +1,13 @@
+package com.smartqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartqueueApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/smartqueue/SmartqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
